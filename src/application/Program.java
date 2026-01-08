2bd2fa6354e7630ad8b30016bc530adaf702df4a
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-08 18:13:26 -0300

    seller entity class

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+
+public class Program {
+    public static void main(String[] args) {
+        Department depart = new Department(1, "Books");
+        System.out.println(depart);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 1852.00, depart);
+        System.out.println(seller);
+    }
+}
