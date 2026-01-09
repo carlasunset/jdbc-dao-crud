8209cd533d65f568ca1267806c7d41ca1cb91388
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-09 14:13:58 -0300

    refactoring findByID implementation to close statement and resultSet

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,5 +1,6 @@
 package model.dao.impl;
 
+import db.DB;
 import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -49,8 +50,13 @@ public class DepartmentDaoJDBC implements DepartmentDao {
             }
             return null;
 
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(preparedStatement);
+            DB.closeResultSet(resultSet);
         }
     }
 
